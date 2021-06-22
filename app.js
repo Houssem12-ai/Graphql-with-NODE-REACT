7b4a610e7e2c7356625282c06ed62cabf2891872
Hossprog <hossprog@gmail.com>
2021-06-22 11:44:22 +0100

    firs commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const { graphqlHTTP } = require('express-graphql');
+
+const app = express();
+
+
+app.use('/graphql',graphqlHTTP({}));//GraphQL middleware requires options.
+
+const PORT = 4014;
+app.listen(PORT, () => {
+    console.log('server is up and running')
+})
